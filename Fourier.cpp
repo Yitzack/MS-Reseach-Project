6883c0291fc9bc1ac2167a2d699d6240bbe49bcd
Yitzack <Yitzack@users.noreply.github.com>
2014-11-20 09:38:49 -0600

    Update Fourier.cpp

--- Fourier.cpp
+++ Fourier.cpp
@@ -190,7 +190,7 @@ long double Integrate2(long double Par[6], long double SelfPPar[3], long double 
 long double Integrate1(long double Par[6], long double SelfPPar[3], long double SelfEPar[5], long double E, long double z)
 {
 	Par[4] = E;
-	long double Value = Spectral(Par, SelfPPar, SelfEPar, E, 3);
+	long double Value = Spectral(Par, SelfPPar, SelfEPar, E, 0);
 	if(abs(z-.5) <= .005)
 		cout << E << " " << Value << endl;
 	return(2.*M_PI*exp(-E*z)*Value);	//return the best estimate of the integral on the interval*/
