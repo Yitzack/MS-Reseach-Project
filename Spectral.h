425f585c4586e9897114d0187d9f3da05d6de447
Yitzack <Yitzack@users.noreply.github.com>
2014-12-01 22:04:59 -0600

    Update Spectral.h
    Eliminate extra inputs into Integrate1 that were getting discarded anyways

--- Spectral.h
+++ Spectral.h
@@ -8,7 +8,7 @@ using namespace std;
 inline long double Energy(long double, long double, long double, long double);	//Returns sqrt(M^2+p^2+k^2-pk cos(theta))
 long double ReInt(long double[6], long double, long double, int);	//Returns the real part of the integrad from 2p_0 to infinity
 long double ImInt(long double[6], long double, long double, int);	//Returns the imaginary part of the integrad from 2p_0 to infinity
-long double Integrate1(long double, long double, long double, long double, long double(*)(long double[6], long double, long double, int), long double[6], long double, int);	//Integrates the part that it is told to integrate. It uses the difference in evaluating the trapaziod rule and Simpson's rule to pick the points that it integrate between. Since the Gaussian quadrature is very accuate for using very few points (2n-1 order polynomial accurate with n points), it then returns the Gaussian quadrature for 3 points on that subinterval.
+long double Integrate1(long double(*)(long double[6], long double, long double, int), long double[6], long double, int);	//Integrates the part that it is told to integrate. It uses the difference in evaluating the trapaziod rule and Simpson's rule to pick the points that it integrate between. Since the Gaussian quadrature is very accuate for using very few points (2n-1 order polynomial accurate with n points), it then returns the Gaussian quadrature for 3 points on that subinterval.
 long double Integrate2(long double, long double, long double, long double, long double, long double, long double(*)(long double[6], long double, long double, int), long double[6], int);	//Contains more brains than Integrate1() as it will need to divide the integral into 2 parts and pass the endpoints down for faster times but it uses the same algorithm to acheive its results.
 long double Self_Energy(int, long double); //Returns Sigma*(a*Lambda^2/(Lambda^2+P^2)+(1-a)exp(-P^2/sigma)), this is my choice of function for the self-energy
 inline long double ReProp(long double[6], long double, long double, int);	//Returns the real part of the propagator
@@ -336,7 +336,7 @@ long double Integrate2(long double a, long double b, long double F_a, long doubl
 {
 	long double F_c = Integrand(Parameters, c, (a+b)/2., Temp);	//Inital end points of the boundary
 	long double F_d = Integrand(Parameters, d, (a+b)/2., Temp);
-	long double F_ave = Integrate1(c, d, F_c, F_d, Integrand, Parameters, a/2.+b/2., Temp);	//Evaluate k integral at (a+b)/2
+	long double F_ave = Integrate1(Integrand, Parameters, a/2.+b/2., Temp);	//Evaluate k integral at (a+b)/2
 
 	long double Trapazoid = (F_a+F_b)*(b-a)/2.;		//Trapazoid rule
 	long double Simpsons = (F_a+F_ave*4.+F_b)*(b-a)/6.;	//Simpson's rule
@@ -357,18 +357,18 @@ long double Integrate2(long double a, long double b, long double F_a, long doubl
 
 			F_c = Integrand(Parameters, c, x1[i], Temp);	//Inital end points of the boundary
 			F_d = Integrand(Parameters, d, x1[i], Temp);
-			F_a += Integrate1(c, d, F_c, F_d, Integrand, Parameters, x1[i], Temp)*w[i+1];	//Evaluate k integral at x1
+			F_a += Integrate1(Integrand, Parameters, x1[i], Temp)*w[i+1];	//Evaluate k integral at x1
 
 			F_c = Integrand(Parameters, c, x3[i], Temp);	//Inital end points of the boundary
 			F_d = Integrand(Parameters, d, x3[i], Temp);
-			F_b += Integrate1(c, d, F_c, F_d, Integrand, Parameters, x3[i], Temp)*w[i+1];	//Evaluate k integral at x3
+			F_b += Integrate1(Integrand, Parameters, x3[i], Temp)*w[i+1];	//Evaluate k integral at x3
 		}
 
 		return((F_a+w[0]*F_ave+F_b)*(b-a)/(2.));	//return the best estimate of the integral on the interval*/
 	}
 }
 
-long double Integrate1(long double a, long double b, long double F_a, long double F_b, long double(*Integrand)(long double[6], long double, long double, int), long double Parameters[6], long double theta, int Temp)
+long double Integrate1(long double(*Integrand)(long double[6], long double, long double, int), long double Parameters[6], long double theta, int Temp)
 {
 	long double Disp[] = {0.06342068498268678602883,  0.1265859972696720510680, 0.1892415924618135864853,  0.2511351786125772735072, 0.3120175321197487622079,  0.3716435012622848888637, 0.4297729933415765246586,  0.4861719414524920421770, 0.5406132469917260665582,  0.5928776941089007124559, 0.6427548324192376640569,  0.6900438244251321135048, 0.7345542542374026962137,  0.7761068943454466350181, 0.8145344273598554315395,  0.8496821198441657010349, 0.8814084455730089100370,  0.9095856558280732852130, 0.9341002947558101490590,  0.9548536586741372335552, 0.9717622009015553801400,  0.9847578959142130043593, 0.9937886619441677907601,  0.9988201506066353793618};	//Dispacement from center for Gauss-Legendre integration
 	long double w[] = {0.06346328140479059771825, 0.06333550929649174859084, 0.06295270746519569947440, 0.06231641732005726740108, 0.06142920097919293629683, 0.06029463095315201730311, 0.05891727576002726602453, 0.05730268153018747548516, 0.05545734967480358869043, 0.05338871070825896852794, 0.05110509433014459067462, 0.04861569588782824027765, 0.04593053935559585354250, 0.04306043698125959798835, 0.04001694576637302136861, 0.03681232096300068981947, 0.03345946679162217434249, 0.02997188462058382535069, 0.02636361892706601696095, 0.02264920158744667649877, 0.01884359585308945844445, 0.01496214493562465102958, 0.01102055103159358049751, 0.007035099590086451473451, 0.003027278988922905077481};	//Weight of the function at Disp
@@ -376,7 +376,8 @@ long double Integrate1(long double a, long double b, long double F_a, long doubl
 	long double wLa[] = {0.07496328305102102808055, 0.1745735743605928864303, 0.2745074833881225250022, 0.3747323102655645620060, 0.4753412526072084401161, 0.5764380939967183636147, 0.6781307242364945406823, 0.7805307978511547593175, 0.8837542316062452388883, 0.9879219194279636096671, 1.0931605619330277996916, 1.1996035979670979427973, 1.3073922479469277349326, 1.416676687469297701993, 1.5276173754408796787012, 1.640386566702889623924, 1.7551700457872174635214, 1.8721691266543402861779, 1.9916029736088098866132, 2.1137113117669909276048, 2.2387576123844772725684, 2.3670328602831611098048, 2.4988600392644108123394, 2.6345995091430390709, 2.7746554982525006307172, 2.9194840027576204632431, 3.0696024758091833914472, 3.2256018156600758204608, 3.3881613374746331979827, 3.5580676615951707296054, 3.7362388067183244743069, 3.9237552950635210172968, 4.1219008467729629867363, 4.3322164077399479741288, 4.5565730632309056055423, 4.7972722621195591678357, 5.057186469320242487569, 5.3399612774797865633198, 5.6503138450512931300331, 5.9944877492232503537552, 6.3809726096501927329094, 6.8216946862388774056326, 7.3340972531892936469048, 7.9450326451948326187906, 8.6987143462393085933469, 9.6750102652900375180015, 11.039313738067347840094, 13.220456867750092021034, 17.982575250664959108273};	//Weight of the function at DispLa
 	long double x1[49];	//These are the two other points required for 5th order Gaussian quadrature for this interval
 	long double x3[24];	//x1 is extended for use in Gauss-Laguerre integration
-	long double F_ave;
+	long double F_a, F_b, F_ave;
+	long double a, b;
 	long double Answer = 0;
 	int i, j, start = 0;
 
