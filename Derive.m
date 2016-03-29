5c89a8321404c173e65764deadf79f8c2d835df3
Yitzack <Yitzack@users.noreply.github.com>
2016-03-28 22:44:50 -0500

    Update Derive.m
    Need to have all variables defined. In this case STable was replaced by Spectral in only one place

--- Derive.m
+++ Derive.m
@@ -1,17 +1,17 @@
 Spectral=Import["~/Physics/Research/MS/Spectral.1.xml"] (*Import the table for interpolation extrapolation*)
-STable1 = STable[[1]]; (*Extract the subtables*)
-STable2 = STable[[2]];
-STable3 = STable[[3]];
+Spectral1 = Spectral[[1]]; (*Extract the subtables*)
+Spectral2 = Spectral[[2]];
+Spectral3 = Spectral[[3]];
 
 s[j_] := If[j <= 425, N[(j - 400)/10], If[j <= 626, 2.5040308 + (j - 426)/200, If[j <= 667, 3.55 + (11 (j - 627))/800, 4.1 + (j - 667)/10]]]  (*Remove the enumeration of positive s values*)
-For[i = 1, i <= 348176, i++, STable3[[i]][[1]][[2]] = s[STable3[[i]][[1]][[2]]]^2; STable3[[i]][[1]][[1]] = 4/5 STable3[[i]][[1]][[1]]]
+For[i = 1, i <= 348176, i++, Spectral3[[i]][[1]][[2]] = s[Spectral3[[i]][[1]][[2]]]^2; Spectral3[[i]][[1]][[1]] = 4/5 Spectral3[[i]][[1]][[1]]]
 
 e1=Spectral[[1;;462,1]][[All,2]] (*Extract the (invariant mass)^2 abscissa*)
 Clear[i] (*Prepare i for use in the derivitive calculation*)
 
-f = Interpolation[STable1, Method -> "Spline"] (*Turn the table into an interpolation*)
-g = Interpolation[STable2, Method -> "Spline"]
-h = Interpolation[STable3, Method -> "Spline"]
+f = Interpolation[Spectral1, Method -> "Spline"] (*Turn the table into an interpolation*)
+g = Interpolation[Spectral2, Method -> "Spline"]
+h = Interpolation[Spectral3, Method -> "Spline"]
 List1 = D[f[i, j], i] /. i -> Range[0, 13] /. j -> Range[0, 400]; (*Calculate the derivitives*)
 List2 = D[f[i, j], j] /. i -> Range[0, 13] /. j -> Range[0, 400];
 List3 = D[f[i, j], i, j] /. i -> Range[0, 13] /. j -> Range[0, 400];
