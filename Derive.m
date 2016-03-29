bd03db4c954577e2917e2ff06a4b77c962da322c
Yitzack <Yitzack@users.noreply.github.com>
2016-03-28 20:55:35 -0500

    Update Derive.m
    Change to include negative area and fact that region is extremely distorted from a simple grid.

--- Derive.m
+++ Derive.m
@@ -1,79 +1,27 @@
-Spectral=Import["~/T.0/Spectral.xml"] (*Import the table for interpolation extrapolation*)
-e1=Spectral[[1;;462,1]][[All,2]] (*Extract the energy abscissa*)
+Spectral=Import["~/Physics/Research/MS/Spectral.1.xml"] (*Import the table for interpolation extrapolation*)
+STable1 = STable[[1]]; (*Extract the subtables*)
+STable2 = STable[[2]];
+STable3 = STable[[3]];
 
-f=Interpolation[Spectral,Method->"Spline"] (*Turn the table into an interpolation*)
-List1=D[f[P,e],P]/.P->Range[0,600.8,.8]/.e->e1 (*Calculate the derivitives*)
-List2=D[f[P,e],e]/.P->Range[0,600.8,.8]/.e->e1
-List3=D[f[P,e],P,e]/.P->Range[0,600.8,.8]/.e->e1
+s[j_] := If[j <= 425, N[(j - 400)/10], If[j <= 626, 2.5040308 + (j - 426)/200, If[j <= 667, 3.55 + (11 (j - 627))/800, 4.1 + (j - 667)/10]]]  (*Remove the enumeration of positive s values*)
+For[i = 1, i <= 348176, i++, STable3[[i]][[1]][[2]] = s[STable3[[i]][[1]][[2]]]^2; STable3[[i]][[1]][[1]] = 4/5 STable3[[i]][[1]][[1]]]
 
-Export["~/T.0/Spectral.xml",{List1,List2,List3}] (*Export the table*)
+e1=Spectral[[1;;462,1]][[All,2]] (*Extract the (invariant mass)^2 abscissa*)
+Clear[i] (*Prepare i for use in the derivitive calculation*)
 
-Parameters=Table[err,{i,462},{j,4}] (*Declare a table for parameterization of the extrapolation*)
+f = Interpolation[STable1, Method -> "Spline"] (*Turn the table into an interpolation*)
+g = Interpolation[STable2, Method -> "Spline"]
+h = Interpolation[STable3, Method -> "Spline"]
+List1 = D[f[i, j], i] /. i -> Range[0, 13] /. j -> Range[0, 400]; (*Calculate the derivitives*)
+List2 = D[f[i, j], j] /. i -> Range[0, 13] /. j -> Range[0, 400];
+List3 = D[f[i, j], i, j] /. i -> Range[0, 13] /. j -> Range[0, 400];
+List4 = D[g[i, j], i] /. i -> Range[13, 751] /. j -> Range[0, 400];
+List5 = D[g[i, j], j] /. i -> Range[13, 751] /. j -> Range[0, 400];
+List6 = D[g[i, j], i, j] /. i -> Range[13, 751] /. j -> Range[0, 400];
+List7 = D[h[P, e], P] /. P -> Range[0, 600.8, .8] /. e -> e1;
+List8 = D[h[P, e], e] /. P -> Range[0, 600.8, .8] /. e -> e1;
+List9 = D[h[P, e], P, e] /. P -> Range[0, 600.8, .8] /. e -> e1;
 
-For[i = 1, i <= 462, i++, 
-  Parameters[[i]] = {err, a, b, n} /. 
-  FindFit[Transpose[{Spectral[[i ;; ;; 462, 1]][[All, 1]], Spectral[[i ;; ;; 462, 2]]}][[251 ;;]],
-    b P^n, {{a, 1}, b, n}, P, MaxIterations -> 1000]] (*Calculate the parameterization of the extrapolation*)
-For[i = 1, i <= 462, i++, Check[
-  Parameters[[i]] = {1, a, b, n} /. 
-    FindFit[Transpose[{Spectral[[i ;; ;; 462, 1]][[All, 1]], Spectral[[i ;; ;; 462, 2]]}][[251 ;;]], 
-    a + b P^n, {a, {b, Parameters[[i, 3]]}, {n, Parameters[[i, 4]]}}, P, MaxIterations -> 1000], 
-  Parameters[[i, 1]] = err, FindFit::cvmit]]
-
-For[i = 1, i <= 462, i++, 
-  If[Or[Parameters[[i, 1]] == err, Parameters[[i,4]] > 1], 
-    Parameters[[i]] = {err, a, b, n} /. 
-    FindFit[Transpose[{Spectral[[i ;; ;; 462, 1]][[All, 1]], Spectral[[i ;; ;; 462, 2]]}][[251 ;;]], 
-    Spectral[[462 ;; ;; 462, 2]][[752]] + b P^n, {{a, Spectral[[462 ;; ;; 462, 2]][[752]]}, b, n}, P, MaxIterations -> 1000]]] (*Calculate the parameterization of the extrapolation*)
-For[i = 1, i <= 462, i++, 
-  If[Parameters[[i, 1]] == err, 
-    Check[Parameters[[i]] = {1, a, b, n} /. 
-    FindFit[Transpose[{Spectral[[i ;; ;; 462, 1]][[All, 1]], Spectral[[i ;; ;; 462, 2]]}][[251 ;;]], 
-    a + b P^n, {{a, Parameters[[i, 2]]}, {b, Parameters[[i, 3]]}, {n, Parameters[[i, 4]]}}, P, MaxIterations -> 1000], 
-  Parameters[[i, 1]] = err, FindFit::cvmit]]]
-
-Holder = {{err,err,err},{err,err,err}}
-
-For[i = 1, i <= 462, i++,
- If[Or[Parameters[[i, 1]] == err, Parameters[[i,4]] > 1], 
-  Holder[[1]] = {err, a, b, n} /. 
-    FindFit[Transpose[{Spectral[[i ;; ;; 462, 1]][[All, 1]], Spectral[[i ;; ;; 462, 2]]}][[251 ;;]], 
-    b P^n, {{a, 1}, b, n}, P, MaxIterations -> 1000]; 
-  Holder[[1]] = {3, a, b, n} /. 
-    FindFit[Transpose[{Spectral[[i ;; ;; 462, 1]][[All, 1]], Spectral[[i ;; ;; 462, 2]]}][[251 ;;]], 
-    a + b P^n, {a, {b, Holder[[1, 3]]}, {n, Holder[[1, 4]]}}, P, MaxIterations -> 1000];
-  Holder[[2]] = {err, a, b, n} /. 
-    FindFit[Transpose[{Spectral[[i ;; ;; 462, 1]][[All, 1]], Spectral[[i ;; ;; 462, 2]]}][[251 ;;]], 
-    Spectral[[462 ;; ;; 462, 2]][[752]] + b P^n, {{a, Spectral[[462 ;; ;; 462, 2]][[752]]}, b, n}, P, MaxIterations -> 1000];
-  Holder[[2]] = {3, a, b, n} /. 
-    FindFit[Transpose[{Spectral[[i ;; ;; 462, 1]][[All, 1]], Spectral[[i ;; ;; 462, 2]]}][[251 ;;]], 
-    a + b P^n, {{a, Holder[[1, 2]]}, {b, Holder[[1, 3]]}, {n, Holder[[1, 4]]}}, P, MaxIterations -> 1000];
-  If[Holder[[1, 4]] > 1,
-   If[Holder[[2, 4]] > 1,
-    Parameters[[i, 1]] = 4; 
-    Parameters[[i, 2]] = (Holder[[1, 2]] + Holder[[2, 2]])/2.; 
-    Parameters[[i, 3]] = (Holder[[1, 3]] + Holder[[2, 3]])/2.; 
-    Parameters[[i, 4]] = (Holder[[1, 4]] + Holder[[2, 4]])/2.,
-    Parameters[[i]] = Holder[[2]]],
-   If[Holder[[2, 4]] > 1,
-    Parameters[[i]] = Holder[[1]],
-    Parameters[[i, 1]] = 3; 
-    Parameters[[i, 2]] = (Holder[[1, 2]] + Holder[[2, 2]])/2.; 
-    Parameters[[i, 3]] = (Holder[[1, 3]] + Holder[[2, 3]])/2.; 
-    Parameters[[i, 4]] = (Holder[[1, 4]] + Holder[[2, 4]])/2.]]]]
-    
-a=Interpolation[Transpose[{e1,Parameters[[All,2]]}],Method->"Spline"](*Interpolation of parameters over energy*)
-b=Interpolation[Transpose[{e1,Parameters[[All,3]]}],Method->"Spline"]
-n=Interpolation[Transpose[{e1,Parameters[[All,4]]}],Method->"Spline"]
-
-List1 = a[e] /. e -> e1 (*Place the parameterization in the list*)
-List2 = D[a[e], e] /. e -> e1 (*Place the derivative of the parameterization in the list*)
-List3 = b[e] /. e -> e1
-List4 = D[b[e], e] /. e -> e1
-List5 = n[e] /. e -> e1
-List6 = D[n[e], e] /. e -> e1
-
-Export["~/T.0/SpectralExtrapolate.xml",{List1,List2,List3,List4,List5,List6}] (*Export the table to a second file. Can't
-go in the first file as it isn't of the same size.*)
+Export["~/Physics/Research/MS/SpectralLarge.1.xml", {List1, List2, List3, List4, List5, List6, List7, List8, List9}] (*Export the table*)
 
 Exit[]
