9c7af4f03f229ae94e6ee9c02032d6ed44730936
Yitzack <Yitzack@users.noreply.github.com>
2016-04-01 13:20:54 -0500

    Update Spectral.cpp
    Allow vacuum width to all temps.

--- Spectral.cpp
+++ Spectral.cpp
@@ -28,9 +28,6 @@ int main(int argc, char* argv[])
 	long double Table[863][3];
 	long double Par[6] = {-127.995280691106, 1.4049344847006076, 1.8, 0, 0, .032};
 
-	if(Temp != 0)
-		Par[5] = 0;
-
 	TPlot << setprecision(18);	//18 digits is the "Number of decimal digits that can be rounded into a floating-point and back without change in the number of decimal digits" for long double.
 
 	#pragma omp parallel for
