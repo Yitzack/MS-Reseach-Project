3b24318a2a6622a56a2c5a78ca3a09a85e0fcbe3
Yitzack <isaac@panther.physics.tamu.edu>
2016-04-06 15:15:25 -0500

    Update Fourier.cpp
    Find all the bugs, maybe. I think I have issues with the jacobian for Table[0] and Table[1].

--- Fourier.cpp
+++ Fourier.cpp
@@ -38,7 +38,7 @@ int main(int argc, char* argv[])
 
 	Init(Table, N, M);
 	ReadIn(Table, N, M, argv[4]);
-	Validate(Table, N, M);
+	//Validate(Table, N, M);
 
 //Debugging code, used for checking the contents of the Table to ensure that it was filled correctly
 	/*const long double EList[] = {0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2, 2.1, 2.2, 2.3, 2.4, 2.5, 2.540308, 2.545308, 2.550308, 2.555308, 2.560308, 2.565308, 2.570308, 2.575308, 2.580308, 2.585308, 2.590308, 2.595308, 2.600308, 2.605308, 2.610308, 2.615308, 2.620308, 2.625308, 2.630308, 2.635308, 2.640308, 2.645308, 2.650308, 2.655308, 2.660308, 2.665308, 2.670308, 2.675308, 2.680308, 2.685308, 2.690308, 2.695308, 2.700308, 2.705308, 2.710308, 2.715308, 2.720308, 2.725308, 2.730308, 2.735308, 2.740308, 2.745308, 2.750308, 2.755308, 2.760308, 2.765308, 2.770308, 2.775308, 2.780308, 2.785308, 2.790308, 2.795308, 2.800308, 2.805308, 2.810308, 2.815308, 2.820308, 2.825308, 2.830308, 2.835308, 2.840308, 2.845308, 2.850308, 2.855308, 2.860308, 2.865308, 2.870308, 2.875308, 2.880308, 2.885308, 2.890308, 2.895308, 2.900308, 2.905308, 2.910308, 2.915308, 2.920308, 2.925308, 2.930308, 2.935308, 2.940308, 2.945308, 2.950308, 2.955308, 2.960308, 2.965308, 2.970308, 2.975308, 2.980308, 2.985308, 2.990308, 2.995308, 3.000308, 3.005308, 3.010308, 3.015308, 3.020308, 3.025308, 3.030308, 3.035308, 3.040308, 3.045308, 3.050308, 3.055308, 3.060308, 3.065308, 3.070308, 3.075308, 3.080308, 3.085308, 3.090308, 3.095308, 3.100308, 3.105308, 3.110308, 3.115308, 3.120308, 3.125308, 3.130308, 3.135308, 3.140308, 3.145308, 3.150308, 3.155308, 3.160308, 3.165308, 3.170308, 3.175308, 3.180308, 3.185308, 3.190308, 3.195308, 3.200308, 3.205308, 3.210308, 3.215308, 3.220308, 3.225308, 3.230308, 3.235308, 3.240308, 3.245308, 3.250308, 3.255308, 3.260308, 3.265308, 3.270308, 3.275308, 3.280308, 3.285308, 3.290308, 3.295308, 3.300308, 3.305308, 3.310308, 3.315308, 3.320308, 3.325308, 3.330308, 3.335308, 3.340308, 3.345308, 3.350308, 3.355308, 3.360308, 3.365308, 3.370308, 3.375308, 3.380308, 3.385308, 3.390308, 3.395308, 3.400308, 3.405308, 3.410308, 3.415308, 3.420308, 3.425308, 3.430308, 3.435308, 3.440308, 3.445308, 3.450308, 3.455308, 3.460308, 3.465308, 3.470308, 3.475308, 3.480308, 3.485308, 3.490308, 3.495308, 3.500308, 3.505308, 3.510308, 3.515308, 3.520308, 3.525308, 3.530308, 3.535308, 3.540308, 3.55, 3.56375, 3.5775, 3.59125, 3.605, 3.61875, 3.6325, 3.64625, 3.66, 3.67375, 3.6875,  3.70125, 3.715, 3.72875, 3.7425, 3.75625, 3.77, 3.78375, 3.7975, 3.81125, 3.825, 3.83875, 3.8525, 3.86625, 3.88, 3.89375, 3.9075, 3.92125, 3.935, 3.94875, 3.9625, 3.97625, 3.99, 4.00375, 4.0175, 4.03125, 4.045, 4.05875, 4.0725, 4.08625, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 9, 9.1, 9.2, 9.3, 9.4, 9.5, 9.6, 9.7, 9.8, 9.9, 10, 10.1, 10.2, 10.3, 10.4, 10.5, 10.6, 10.7, 10.8, 10.9, 11, 11.1, 11.2, 11.3, 11.4, 11.5, 11.6, 11.7, 11.8, 11.9, 12, 12.1, 12.2, 12.3, 12.4, 12.5, 12.6, 12.7, 12.8, 12.9, 13, 13.1, 13.2, 13.3, 13.4, 13.5, 13.6, 13.7, 13.8, 13.9, 14, 14.1, 14.2, 14.3, 14.4, 14.5, 14.6, 14.7, 14.8, 14.9, 15, 15.1, 15.2, 15.3, 15.4, 15.5, 15.6, 15.7, 15.8, 15.9, 16, 16.1, 16.2, 16.3, 16.4, 16.5, 16.6, 16.7, 16.8, 16.9, 17, 17.1, 17.2, 17.3, 17.4, 17.5, 17.6, 17.7, 17.8, 17.9, 18, 18.1, 18.2, 18.3, 18.4, 18.5, 18.6, 18.7, 18.8, 18.9, 19, 19.1, 19.2, 19.3, 19.4, 19.5, 19.6, 19.7, 19.8, 19.9, 20, 20.1, 20.2, 20.3, 20.4, 20.5, 20.6, 20.7, 20.8, 20.9, 21., 21.1, 21.2, 21.3, 21.4, 21.5, 21.6, 21.7, 21.8, 21.9, 22., 22.1, 22.2, 22.3, 22.4, 22.5, 22.6, 22.7, 22.8, 22.9, 23., 23.1, 23.2, 23.3, 23.4, 23.5};
@@ -50,10 +50,21 @@ int main(int argc, char* argv[])
 	}//*/
 
 //Debugging code, used to ensure that the interpolations or othe approximations are working correctly
-	/*for(long double j = 0; j <= 100; j+=.4)
+	/*for(long double j = 0; j <= 0; j+=.4)
 	{
-		for(long double i = 2.5; i <= 3.55; i+=.0025)
-			cout << j << " " << i << " " << Spectral(Table, Extrapolation, i, j) << endl;
+		for(long double i = 0; i <= 23.5; i+=.0002)
+			cout << j << " " << i << " " << Spectral(Table, i, j, -1, 2) << endl;
+		cout << endl;
+	}//*/
+	/*for(long double j = 0; j <= 25; j+=.4)
+	{
+		for(long double i = 0; i < 400; i+=.25)
+		{
+			if(j < 13)
+				cout << j << " " << i << " " << Spectral(Table, i, j, -1, 0) << endl;
+			if(j >= 13)
+				cout << j << " " << i << " " << Spectral(Table, i, j, -1, 1) << endl;
+		}
 		cout << endl;
 	}//*/
 
@@ -66,7 +77,6 @@ int main(int argc, char* argv[])
 	}//*/
 
 //The actual program
-	#pragma omp parallel for private(z, tau, holder)
 	for(int i = 290*iProcess/Total; i <= 290*(iProcess+1)/Total; i++)
 	{
 		z = .3+i*.02;
@@ -101,7 +111,7 @@ long double Correlator(long double(*Kernal)(long double***[], long double, long 
 	{
 		for(i = 0; i < 8; i++)
 		{
-			b = pow(3.040308,2) - distance[i];
+			b = 3.040308 - distance[i];
 			F_a = F_b = 0;	//Start integration at 0
 			for(j = 0; j < 24; j++)
 			{
@@ -116,7 +126,7 @@ long double Correlator(long double(*Kernal)(long double***[], long double, long 
 			a = b;
 		}
 
-		b = pow(3.040308,2);
+		b = 3.040308;
 		F_a = F_b = 0;	//Start integration at 0
 		for(j = 0; j < 24; j++)
 		{
@@ -132,7 +142,7 @@ long double Correlator(long double(*Kernal)(long double***[], long double, long 
 
 		for(i = 7; i >= 0; i--)
 		{
-			b = pow(3.040308,2) + distance[i];
+			b = 3.040308 + distance[i];
 			F_a = F_b = 0;	//Start integration at 0
 			for(j = 0; j < 24; j++)
 			{
@@ -147,7 +157,7 @@ long double Correlator(long double(*Kernal)(long double***[], long double, long 
 			a = b;
 		}
 
-		b = 12.96;
+		b = 3.6;
 		F_a = F_b = 0;	//Start integration at 0
 		for(j = 0; j < 24; j++)
 		{
@@ -163,7 +173,7 @@ long double Correlator(long double(*Kernal)(long double***[], long double, long 
 
 		for(i = 7; i >= 0; i--)
 		{
-			b = 12.96 + distance[i];
+			b = 3.6 + distance[i];
 			F_a = F_b = 0;	//Start integration at 0
 			for(j = 0; j < 24; j++)
 			{
@@ -178,7 +188,7 @@ long double Correlator(long double(*Kernal)(long double***[], long double, long 
 			a = b;
 		}
 
-		b = 16;
+		b = 4;
 		F_a = F_b = 0;	//Start integration at 0
 		for(j = 0; j < 24; j++)
 		{
@@ -192,7 +202,7 @@ long double Correlator(long double(*Kernal)(long double***[], long double, long 
 		Answer += (F_a+w[0]*F_ave+F_b)*(b-a)/(2.);
 		a = b;
 
-		b = 100;
+		b = 10;
 		F_a = F_b = 0;	//Start integration at 0
 		for(j = 0; j < 24; j++)
 		{
@@ -206,7 +216,7 @@ long double Correlator(long double(*Kernal)(long double***[], long double, long 
 		Answer += (F_a+w[0]*F_ave+F_b)*(b-a)/(2.);
 		a = b;
 
-		b = 552.25;
+		b = 23.6;
 		F_a = F_b = 0;	//Start integration at 0
 		for(j = 0; j < 24; j++)
 		{
@@ -348,7 +358,7 @@ long double Spatial1(long double*** Table[], long double j, long double z, int T
 	return(Answer);	//return the best estimate of the integral on the interval*/
 }
 
-long double Spatial2(long double*** Table[], long double s, long double z, int Temp)
+long double Spatial2(long double*** Table[], long double roots, long double z, int Temp)
 {
 	long double Disp[] = {0.06342068498268678602883,  0.1265859972696720510680, 0.1892415924618135864853,  0.2511351786125772735072, 0.3120175321197487622079,  0.3716435012622848888637, 0.4297729933415765246586,  0.4861719414524920421770, 0.5406132469917260665582,  0.5928776941089007124559, 0.6427548324192376640569,  0.6900438244251321135048, 0.7345542542374026962137,  0.7761068943454466350181, 0.8145344273598554315395,  0.8496821198441657010349, 0.8814084455730089100370,  0.9095856558280732852130, 0.9341002947558101490590,  0.9548536586741372335552, 0.9717622009015553801400,  0.9847578959142130043593, 0.9937886619441677907601,  0.9988201506066353793618};	//Dispacement from center
 	long double w[] = {0.06346328140479059771825, 0.06333550929649174859084, 0.06295270746519569947440, 0.06231641732005726740108, 0.06142920097919293629683, 0.06029463095315201730311, 0.05891727576002726602453, 0.05730268153018747548516, 0.05545734967480358869043, 0.05338871070825896852794, 0.05110509433014459067462, 0.04861569588782824027765, 0.04593053935559585354250, 0.04306043698125959798835, 0.04001694576637302136861, 0.03681232096300068981947, 0.03345946679162217434249, 0.02997188462058382535069, 0.02636361892706601696095, 0.02264920158744667649877, 0.01884359585308945844445, 0.01496214493562465102958, 0.01102055103159358049751, 0.007035099590086451473451, 0.003027278988922905077481};	//Weight of data point
@@ -371,10 +381,10 @@ long double Spatial2(long double*** Table[], long double s, long double z, int T
 			x1[i] = (b+a-Disp[i]*(b-a))/2.;	//Actual evaluation points
 			x3[i] = (b+a+Disp[i]*(b-a))/2.;
 
-			F_a += Spectral(Table, s, x1[i], z, 2)*2./(x1[i]*x1[i]+s)*cos(z*x1[i])*w[i+1];	//Evaluate k integral at x1
-			F_b += Spectral(Table, s, x3[i], z, 2)*2./(x3[i]*x3[i]+s)*cos(z*x3[i])*w[i+1];	//Evaluate k integral at x3
+			F_a += Spectral(Table, roots, x1[i], z, 2)*4.*roots/(x1[i]*x1[i]+pow(roots,2))*cos(z*x1[i])*w[i+1];	//Evaluate k integral at x1
+			F_b += Spectral(Table, roots, x3[i], z, 2)*4.*roots/(x3[i]*x3[i]+pow(roots,2))*cos(z*x3[i])*w[i+1];	//Evaluate k integral at x3
 		}
-		F_ave = Spectral(Table, s, a/2.+b/2., z, 2)*2./(pow(a/2.+b/2.,2)+s)*cos(z*(a/2.+b/2.));
+		F_ave = Spectral(Table, roots, a/2.+b/2., z, 2)*4.*roots/(pow(a/2.+b/2.,2)+pow(roots,2))*cos(z*(a/2.+b/2.));
 		Answer += (F_a+w[0]*F_ave+F_b)*(b-a)/(2.);
 		a = b;
 	}	//For the bulk of the integral where either the result is well approximated by either the finite or zero width analytic result
@@ -386,16 +396,16 @@ long double Spatial2(long double*** Table[], long double s, long double z, int T
 		x1[i] = (b+a-Disp[i]*(b-a))/2.;	//Actual evaluation points
 		x3[i] = (b+a+Disp[i]*(b-a))/2.;
 
-		F_a += Spectral(Table, s, x1[i], z, 2)*2./(x1[i]*x1[i]+s)*cos(z*x1[i])*w[i+1];	//Evaluate k integral at x1
-		F_b += Spectral(Table, s, x3[i], z, 2)*2./(x3[i]*x3[i]+s)*cos(z*x3[i])*w[i+1];	//Evaluate k integral at x3
+		F_a += Spectral(Table, roots, x1[i], z, 2)*4.*roots/(x1[i]*x1[i]+pow(roots,2))*cos(z*x1[i])*w[i+1];	//Evaluate k integral at x1
+		F_b += Spectral(Table, roots, x3[i], z, 2)*4.*roots/(x3[i]*x3[i]+pow(roots,2))*cos(z*x3[i])*w[i+1];	//Evaluate k integral at x3
 	}
-	F_ave = Spectral(Table, s, a/2.+b/2., z, 2)*2./(pow(a/2.+b/2.,2)+s)*cos(z*(a/2.+b/2.));
+	F_ave = Spectral(Table, roots, a/2.+b/2., z, 2)*4.*roots/(pow(a/2.+b/2.,2)+pow(roots,2))*cos(z*(a/2.+b/2.));
 	Answer += (F_a+w[0]*F_ave+F_b)*(b-a)/(2.);//*/
 
 	return(Answer);	//return the best estimate of the integral on the interval*/
 }
 
-long double Euclidean(long double*** Table[], long double s, long double tau, int Temp)
+long double Euclidean(long double*** Table[], long double roots, long double tau, int Temp)
 {
 	long double T; //T_c = .196GeV = 196MeV
 
@@ -418,7 +428,7 @@ long double Euclidean(long double*** Table[], long double s, long double tau, in
 	if(tau > 1./(2.*T))
 		return(0);
 
-	return(cosh(sqrt(s)*(tau-1./(2.*T)))/sinh(sqrt(s)/(2.*T))*Spectral(Table, s, 0, 0, 2)/(4.*M_PI*sqrt(s)));	//return the integral for vacuum from 0 to infinity
+	return(cosh(roots*(tau-1./(2.*T)))/sinh(roots/(2.*T))*Spectral(Table, roots, 0, 0, 2)/(2.*M_PI));	//return the integral for vacuum from 0 to infinity
 }
 
 long double Spectral(long double*** Table[], long double E, long double p, long double z, int Specify)
@@ -430,19 +440,18 @@ long double Spectral(long double*** Table[], long double E, long double p, long 
 	if(Specify == 2)
 	{
 		t = p/.8;	//returns the p index with the fractional part
-		u;
-		if(sqrt(E) < 2.5)	//These are to give the fractional distance from one E-point to the next+the index
-			u = sqrt(E)/.1;
-		else if(sqrt(E) < 2.540308)
-			u = 25+(sqrt(E)-2.5)/.040308;
-		else if(sqrt(E) < 3.540308)
-			u = 26+(sqrt(E)-2.540308)/.005;
-		else if(sqrt(E) < 3.55)
-			u = 226+(sqrt(E)-3.540308)/.009692;
-		else if(sqrt(E) < 4.1)
-			u = 227+(sqrt(E)-3.55)/.01375;
+		if(E < 2.5)	//These are to give the fractional distance from one E-point to the next+the index
+			u = E/.1;
+		else if(E < 2.540308)
+			u = 25+(E-2.5)/.040308;
+		else if(E < 3.540308)
+			u = 26+(E-2.540308)/.005;
+		else if(E < 3.55)
+			u = 226+(E-3.540308)/.009692;
+		else if(E < 4.1)
+			u = 227+(E-3.55)/.01375;
 		else
-			u = 267+(sqrt(E)-4.1)/.1;
+			u = 267+(E-4.1)/.1;
 		i = t;	//p index in the Table
 		j = u;	//E index in the Table
 		t -= i;	//Removes the index leaving the fractional part
@@ -654,25 +663,25 @@ void ReadIn(long double*** Table[], int N[], int M[], char* FileReadIn)
 			{
 				for(k = 0; k < M[i]; k++)
 				{
-					File >> Table[i][j][k][1];
-					if(i == 3)
+					File >> Table[i][j][k][m+1];
+					if(i == 2)
 					{
 						if(m == 0 || m == 2)
-							Table[i][j][k][m] *= .8;	//*=dP/di
+							Table[i][j][k][m+1] *= .8;	//*=dP/di
 						if(m == 1 || m == 2)
 						{
-							if(j < 25)
-								Table[i][j][k][m] *= (1.+2.*j)/100;
-							else if(j < 26)
-								Table[i][j][k][m] *= pow(.040308,2)+5*.040308;
-							else if(j < 226)
-								Table[i][j][k][m] *= (965.1232+2.*j)/40000;
-							else if(j < 227)
-								Table[i][j][k][m] *= pow(.009692,2)+2*3.540308*.009692;
-							else if(j < 267)
-								Table[i][j][k][m] *= 121.*(697./11.+2.*j)/640000;
+							if(k < 25)
+								Table[i][j][k][m+1] *= .1;
+							else if(k < 26)
+								Table[i][j][k][m+1] *= .040308;
+							else if(k < 226)
+								Table[i][j][k][m+1] *= .005;
+							else if(k < 227)
+								Table[i][j][k][m+1] *= .009692;
+							else if(k < 267)
+								Table[i][j][k][m+1] *= .01375;
 							else
-								Table[i][j][k][m] *= j/50.-4.51;
+								Table[i][j][k][m+1] *= .1;
 						}
 					}
 				}
