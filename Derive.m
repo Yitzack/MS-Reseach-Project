472d009fed163845b64ed3799c69a6f3c8d0b7c0
Yitzack <isarver@physics.tamu.edu>
2016-11-22 23:31:21 -0600

    Update Derive.m and XMLScript
    Updates allow automatic flow of data from Spectral.cpp etc through Derive.m and get it ready for Dispersion.cpp. These files are ready for return back to the cluster for Dispersion to do its magic. Too bad Dispersion isn't ready to run yet.

--- Derive.m
+++ Derive.m
@@ -1,4 +1,6 @@
-Master = Import["~/Physics/Research/MS/Spectralcc.0.xml"]; (*Import the table for interpolation extrapolation*)
+input = $CommandLine
+
+Master = Import[StringJoin[Directory[],"/",input[[Dimensions[input]]],".xml"]]; (*Import the table for interpolation*)
 Sub1 = N[Transpose[{Master[[2,All,1]],Master[[2,All,2,2]]}]];(*Extract the subtables*)
 Sub2 = N[Transpose[{Master[[2,All,1]],Master[[2,All,2,3]]}]];
 e1 = Sub1[[;;465,1,2]]; (*Pull the choice of s from the table*)
@@ -6,13 +8,16 @@ e1 = Sub1[[;;465,1,2]]; (*Pull the choice of s from the table*)
 ImGV1 = Interpolation[Sub1, Method -> "Spline"];
 ImGV2 = Interpolation[Sub2, Method -> "Spline"];
 
-List1 = D[ImGV1[P, e], P] /. P -> Range[0, 600.8, .8] /. e -> e1;
-List2 = 2*Sqrt[e]*D[ImGV1[P, e], e] /. P -> Range[0, 600.8, .8] /. e -> e1; (*2*Sqrt[e]* is to change the derivative from d/ds to d/d(sqrt(s))*)
-List3 = 2*Sqrt[e]*D[ImGV1[P, e], P, e] /. P -> Range[0, 600.8, .8] /. e -> e1;
-List4 = D[ImGV2[P, e], P] /. P -> Range[0, 600.8, .8] /. e -> e1;
-List5 = 2*Sqrt[e]*D[ImGV2[P, e], e] /. P -> Range[0, 600.8, .8] /. e -> e1;
-List6 = 2*Sqrt[e]*D[ImGV2[P, e], P, e] /. P -> Range[0, 600.8, .8] /. e -> e1;
+List1 = Flatten[D[ImGV1[P, e], P] /. P -> Range[0, 600.8, .8] /. e -> e1];
+List2 = Flatten[2*Sqrt[e]*D[ImGV1[P, e], e] /. P -> Range[0, 600.8, .8] /. e -> e1]; (*2*Sqrt[e]* is to change the derivative from d/ds to d/d(sqrt(s))*)
+List3 = Flatten[2*Sqrt[e]*D[ImGV1[P, e], P, e] /. P -> Range[0, 600.8, .8] /. e -> e1];
+List4 = Flatten[D[ImGV2[P, e], P] /. P -> Range[0, 600.8, .8] /. e -> e1];
+List5 = Flatten[2*Sqrt[e]*D[ImGV2[P, e], e] /. P -> Range[0, 600.8, .8] /. e -> e1];
+List6 = Flatten[2*Sqrt[e]*D[ImGV2[P, e], P, e] /. P -> Range[0, 600.8, .8] /. e -> e1];
 
-Export["~/Physics/Research/MS/Spectralcc.0.xml", {List1,List2,List3,List4,List5,List6}] (*Export the table*)
+Print[StringJoin[Directory[],"/",input[[Dimensions[input]]],"ImGV1.csv"]]
+Export[StringJoin[Directory[],"/",input[[Dimensions[input]]],"ImGV1.csv"], Transpose[{List1,List2,List3}]] (*Export the table*)
+Print[StringJoin[Directory[],"/",input[[Dimensions[input]]],"ImGV2.csv"]]
+Export[StringJoin[Directory[],"/",input[[Dimensions[input]]],"ImGV2.csv"], Transpose[{List4,List5,List6}]]
 
 Exit[]
