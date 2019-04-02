1008efbfa7a914ec67bff3cef4dd3c3659960fb5
Yitzack <Yitzack@users.noreply.github.com>
2019-04-02 15:09:53 -0500

    Update Spectral.cpp
    Update the filename scheme

--- Spectral.cpp
+++ Spectral.cpp
@@ -23,7 +23,7 @@ int main(int argc, char* argv[])
 	char File[30] = "Spectralbb.";  //Name of the file
 #endif
 #ifdef CC
-     	char File[30] = "Spectralcc_gT_Vac.";  //Name of the file
+     	char File[30] = "Spectralcc_gT_MP.";  //Name of the file
 #endif
 #ifdef RIEK
      	char File[30] = "SpectralccRiek.";  //Name of the file
