4fcd94e1eefa27df42accb4db620e159dcfdc595
Yitzack <isarver@physics.tamu.edu>
2022-05-26 15:00:06 -0500

    Update to ReSpectral.m and add README.md in Real Contibutions directory
    ReSpectral.m got new comments

---
+++ Real Contributions/README.md
@@ -0,0 +1,13 @@
+# Real Contribtions
+
+Mathematica used to be fast enough on the Cyclotron Institute's cluster to run this. The College of Science only has 40 licenses to share now. That's not enough to keep the cluster going. You'll probably want to convert this C++. Also C++ runs circles around Mathmatica anyways. Its a question of if you want to spend your time developing correct code or do you want spend your time waiting on correct code to complete.
+
+## Execute Directions
+
+math -script ProcessID Temprature Version
+
+It will then import "~/data/Tables/Spectralcc24.97.P\_Dep4.Temprature\_Version.csv". It appears to be configured to examine spectral functions with momentum dependence at the moment. This behavior can be altered on line 4.
+
+ProcessID can be any number for which there exists a row of data in the first column of the input file.
+
+Once done, it will export the data to "~/data/ReSpectralcc24.97.P\_Dep4.Temprature\_Version.ProcessID.csv". This behavior can be altered on line 86.
--- Real Contributions/ReSpectral.m
+++ Real Contributions/ReSpectral.m
@@ -2,25 +2,30 @@ assignment = ToExpression[$CommandLine[[4]]];
 Temp = $CommandLine[[5]];
 version = $CommandLine[[6]];
 Master = Import["~/data/Tables/Spectralcc24.97.P_Dep4."<>Temp<>"_"<>version<>".csv"];
-If[Master==$Failed,Exit[]];
+If[Master==$Failed,Exit[]]; (*Did the file import, if not Exit as the rest will be painful to complete*)
 dim=Dimensions[Master][[1]];
-parameters = Select[Master, Dimensions[#][[1]] == 5 &];
-Master = Select[Master, Dimensions[#][[1]] != 5 &];
+parameters = Select[Master, Dimensions[#][[1]] == 5 &]; (*Collect the parameters*)
+Master = Select[Master, Dimensions[#][[1]] != 5 &];	(*Remove the parameters from the rest of the data*)
 upper =Master[[-1,1]]; (*upper edge of the s>=0 table*)
 
+(*Functions to convert calls to s and P in the negative s region to i and j*)
 i1[s_,P_]:=10Sqrt[s+P^2]
 j1[s_,P_]:=10(P-Sqrt[s+P^2])
 i2[s_,P_]:=936/5+Sqrt[s+P^2]
 j2[s_,P_]:=10(P-Sqrt[s+P^2])
+(*Functions to convert calls to any i and j to s and P*)
 P[i_,j_]:=If[j<=150,If[0<=i&&i<=208,(i+j)/10,i+j/10-187.2],If[i>=0,(4i)/5,(Mod[i-1,7]/8-7/8-Floor[i/7])*.8]]
 s[i_,j_]:=If[j<=150,If[0<=i&&i<=208,(-i j)/50-j^2/100,-(j/5)(i-187.2)-j^2/100],If[j<=181,((j-151)/10)^2,If[j<=381,((j-181)/100+3)^2,((j-381)/10+5)^2]]]
+(*Dispersion relation for any f and i and j. There's some funny business around knots of the interpolation. I don't know what the difference is between these two.*)
 ReG[f_,i_,j_]:=(NIntegrate[-f[y,P[i,j]]/(s[i,j]-y),{y,Piecewise[{{-P[i,j]^2,P[i,j]<15}},-30P[i,j]+225],Max[Piecewise[{{-P[i,j]^2,P[i,j]<15}},-30P[i,j]+225],s[i,j]-.001]}]+NIntegrate[(f[s[i,j],P[i,j]]-f[y,P[i,j]])/(s[i,j]-y),{y,s[i,j]-.001,s[i,j]}]+NIntegrate[(f[s[i,j],P[i,j]]-f[y,P[i,j]])/(s[i,j]-y),{y,s[i,j],s[i,j]+.001}]-Re[Integrate[(f[s[i,j],P[i,j]])/(s[i,j]-y),{y,s[i,j]-.001,s[i,j]+.001},PrincipalValue->True]]+NIntegrate[-f[y,P[i,j]]/(s[i,j]-y),{y,s[i,j]+.001,729.096309407015895}])/\[Pi]
 (*ReG[f_,i_,j_]:=(NIntegrate[-f[y,P[i,j]]/(s[i,j]-y),{y,0,Max[0,s[i,j]-.001]}]+NIntegrate[(f[s[i,j],P[i,j]]-f[y,P[i,j]])/(s[i,j]-y),{y,Max[0,s[i,j]-.001],s[i,j]}]+NIntegrate[(f[s[i,j],P[i,j]]-f[y,P[i,j]])/(s[i,j]-y),{y,s[i,j],s[i,j]+.001}]-Re[Integrate[(f[s[i,j],P[i,j]])/(s[i,j]-y),{y,Max[0,s[i,j]-.001],s[i,j]+.001},PrincipalValue->True]]+NIntegrate[-f[y,P[i,j]]/(s[i,j]-y),{y,s[i,j]+.001,729.096309407015895}])/\[Pi]*)
 
+(*Sort the data into tables according to the which conversion between i and j to s and P applies*)
 Dimensions[Sub1=Select[Master[[;;-2,{1,2,5,6,7,8,9}]],#[[2]]<=150&&#[[1]]<=208&]]
 Dimensions[Sub2=Select[Master[[;;-2,{1,2,5,6,7,8,9}]],#[[2]]<=150&&#[[1]]>=208&]]
 Dimensions[Sub3=Select[Master,#[[2]]>150&&#[[1]]<=upper&][[All,3;;]]]
 
+(*Force all numbers to machine real numbers. I think this will force certain subnormal numbers to zero.*)
 If[Temp != "0",
 Sub1=Table[If[MachineNumberQ[N[Sub1[[i,j]]]],N[Sub1[[i,j]]],0],{i,31559},{j,7}];
 dim=Dimensions[Sub2][[1]];
@@ -30,6 +35,7 @@ Sub2=Flatten[Table[{i,j,0,0,0,0,0},{i,208,258},{j,0,150}],1]];
 dim=Dimensions[Sub3][[1]];
 Sub3=Table[If[MachineNumberQ[N[Sub3[[i,j]]]],N[Sub3[[i,j]]],0],{i,dim},{j,7}];
 
+(*Creat the interpolations and how each region connects to the next. Sometimes the above code doesn't make all numbers machine real, so error INterpolation::mspl is silenced*)
 Off[Interpolation::mspl]
 If[Sub1=={},ImG1=0&,ImG1=Interpolation[Transpose[{Transpose[{Sub1[[All,1]],Sub1[[All,2]]}],Sub1[[All,3]]}],Method->"Spline",InterpolationOrder->1]];
 If[Sub1=={},ImGvC1=0&,ImGvC1=Interpolation[Transpose[{Transpose[{Sub1[[All,1]],Sub1[[All,2]]}],Sub1[[All,4]]}],Method->"Spline",InterpolationOrder->1]];
@@ -52,13 +58,16 @@ ImGvL[s_,P_]:=If[s<0&&P<Sqrt[s+P^2]+15,If[s+P^2<=20.8^2,ImGvL1[i1[s,P],j1[s,P]],
 ImGvQ[s_,P_]:=If[s<0&&P<Sqrt[s+P^2]+15,If[s+P^2<=20.8^2,ImGvQ1[i1[s,P],j1[s,P]],ImGvQ2[i2[s,P],j2[s,P]]],If[s>=0,ImGvQ3[P,s],0]];
 ImGV[s_,P_]:=If[s<0&&P<Sqrt[s+P^2]+15,If[s+P^2<=20.8^2,ImGV1[i1[s,P],j1[s,P]],ImGV2[i2[s,P],j2[s,P]]],If[s>=0,ImGV3[P,s],0]];
 
+(*Silence the error messages for 'function is zero everywhere', 'integration failed to converge', 'convergence of integration is proceeding in the wrong direction', 'precision was lost', "integral didn't converge", "integral subdivided to zero interval", "division by zero in interval of analytic function"*)
 Off[NIntegrate::izero,NIntegrate::ncvb,NIntegrate::slwcon,NIntegrate::precw,NIntegrate::eincr,NIntegrate::zeroregion,Integrate::idiv];
 
+(*Calculate the real contributions in the space-like mesons*)
 If[assignment>=0&&Temp!="0",
 	ReGTab1=Table[{assignment,j,ReG[ImGvC,assignment,j],ReG[ImGvL,assignment,j],ReG[ImGvQ,assignment,j],ReG[ImGV,assignment,j]},{j,0,150}];,
 	ReGTab1={};
 ]
 
+(*Calculate the real contributions in the time-like mesons, which may include, 'do nothing'*)
 ReGTab2=If[assignment<=upper,
 	Sub3=Select[Sub3,Abs[#[[1]]-P[assignment,200]]<4&];
 	ImG3=Interpolation[Transpose[{Transpose[{Sub3[[All,1]],Sub3[[All,2]]}],Sub3[[All,3]]}],Method->"Spline"];
@@ -70,8 +79,10 @@ ReGTab2=If[assignment<=upper,
 	,{}
 ]
 
+(*Combine the results*)
 ReGTab=Join[ReGTab1,ReGTab2]
 
+(*Export the results*)
 Export["~/data/ReSpectralcc24.97.P_Dep4."<>Temp<>"_"<>version<>"."<>ToString[assignment]<>".csv",ReGTab]
 
 Exit[]
