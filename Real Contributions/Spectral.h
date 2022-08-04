ba46a79804fc585eb8c13ee5e0f2cb9e44eb050e
Yitzack <isarver@physics.tamu.edu>
2022-08-04 02:11:10 -0500

    Update to Spectral.h in Real Contributions
    Optimised and tuned for accuracy and speed

--- Real Contributions/Spectral.h
+++ Real Contributions/Spectral.h
@@ -314,8 +314,8 @@ long double Dispersion(long double Par[], int Temp, long double k0, long double 
 	long double ImG12 = k0_Int(Par, Temp, k, theta);		//Holder of the ImG12 that belongs to the other half of G12 that is calculated here
 
 	//Extra boundaries that insert extra intervals around peaks. Used a machine learn algorithm of sorts to minimize error to pick these values.
-	long double Boundary_k_k0 = 4.85;
-	long double Range[] = {-Boundary_k_k0, 0, Boundary_k_k0};	//Number of gamma from center
+	long double Boundary_k_k0[] = {0.0491, .421};
+	long double Range[] = {-Boundary_k_k0[1], -Boundary_k_k0[0], 0, Boundary_k_k0[0], Boundary_k_k0[1]};	//Number of gamma from center
 
 	long double F;			//Sum of ordinates*weights
 	long double Answer = 0;	//Results to be returned
@@ -329,13 +329,13 @@ long double Dispersion(long double Par[], int Temp, long double k0, long double 
 	int Principal = 1;	//Principal value integral calculating or not
 
 	Characterize_Dispersion(ParLoc, Temp, k0, k, theta, zero, gamma, Poles);
-	long double Stops[Poles*3+9];		//Extra stops to ensure correctness
+	long double Stops[Poles*5+9];		//Extra stops to ensure correctness
 
 	l = 0;
 	for(i = 0; i < Poles; i++)
 	{
 		if(!isnan(gamma[i]))	//Prevents bad poles from getting in (It would be better to find the source of bad poles and eliminate it)
-			for(j = 0; j < 3; j++)
+			for(j = 0; j < 5; j++)
 			{
 				Stops[l] = zero[i]+gamma[i]*Range[j];	//Extra subintervals required by poles
 				l++;
@@ -432,20 +432,16 @@ long double k0_Int(long double Par[], int Temp, long double k, long double theta
 	else if(Par[4]+pow(Par[3],2) < 0)	//Bad data trap and time saver. The point is supposed to zero energy anyways but got evaluated to non-zero
 		return(0);
 
-#if ORDER == 37	//37th order Gauss-Legendre integration
-	long double Disp[] = {0.1603586456402253758680961, 0.3165640999636298319901173, 0.4645707413759609457172671, 0.6005453046616810234696382, 0.7209661773352293786170959, 0.8227146565371428249789225, 0.9031559036148179016426609, 0.9602081521348300308527788, 0.9924068438435844031890177};	//Displacement from center
-	long double w[] = {8589934592./53335593025., 0.1589688433939543476499564, 0.1527660420658596667788554, 0.1426067021736066117757461, 0.1287539625393362276755158, 0.1115666455473339947160239, 0.09149002162244999946446209, 0.06904454273764122658070826, 0.04481422676569960033283816, 0.01946178822972647703631204};	//Weight
-#elif ORDER == 97	//97th order Gauss-Legendre integration
-	long double Disp[] = {0.06342068498268678602883, 0.1265859972696720510680, 0.1892415924618135864853, 0.2511351786125772735072, 0.3120175321197487622079, 0.3716435012622848888637, 0.4297729933415765246586, 0.4861719414524920421770, 0.5406132469917260665582, 0.5928776941089007124559, 0.6427548324192376640569, 0.6900438244251321135048, 0.7345542542374026962137, 0.7761068943454466350181, 0.8145344273598554315395, 0.8496821198441657010349, 0.8814084455730089100370, 0.9095856558280732852130, 0.9341002947558101490590, 0.9548536586741372335552, 0.9717622009015553801400, 0.9847578959142130043593, 0.9937886619441677907601, 0.9988201506066353793618};	//Displacement from center
-	long double w[] = {0.06346328140479059771825, 0.06333550929649174859084, 0.06295270746519569947440, 0.06231641732005726740108, 0.06142920097919293629683, 0.06029463095315201730311, 0.05891727576002726602453, 0.05730268153018747548516, 0.05545734967480358869043, 0.05338871070825896852794, 0.05110509433014459067462, 0.04861569588782824027765, 0.04593053935559585354250, 0.04306043698125959798835, 0.04001694576637302136861, 0.03681232096300068981947, 0.03345946679162217434249, 0.02997188462058382535069, 0.02636361892706601696095, 0.02264920158744667649877, 0.01884359585308945844445, 0.01496214493562465102958, 0.01102055103159358049751, 0.007035099590086451473451, 0.003027278988922905077481};	//Weight
-#endif
+//9th order Gauss-Legendre integration
+	long double Disp[] = {sqrt(5.-2.*sqrt(10./7.))/3., sqrt(5.+2.*sqrt(10./7.))/3.};	//Displacement from center
+	long double w[] = {128./225., (322.+13.*sqrt(70.))/900., (322.-13.*sqrt(70.))/900.};	//Weight
 	long double a, b;	//Sub-interval limits of integration
 	long double x1, x2;	//Abscissa
 	long double Max;	//Upper limit of integration
 
 	//Extra boundaries that insert extra intervals around peaks. Used a machine learn algorithm of sorts to minimize error to pick these values.
-	long double Boundary_k_k0[] = {0.00865, 0.0267, 0.0491, 0.0985, .421, .802, 1.01, 4.85};
-	long double Range[] = {-Boundary_k_k0[7], -Boundary_k_k0[6], -Boundary_k_k0[5], -Boundary_k_k0[4], -Boundary_k_k0[3], -Boundary_k_k0[2], -Boundary_k_k0[1], -Boundary_k_k0[0], 0, Boundary_k_k0[0], Boundary_k_k0[1], Boundary_k_k0[2], Boundary_k_k0[3], Boundary_k_k0[4], Boundary_k_k0[5], Boundary_k_k0[6], Boundary_k_k0[7]};	//Number of gamma from center
+	long double Boundary_k_k0[] = {.421, 4.85};
+	long double Range[] = {-Boundary_k_k0[1], -Boundary_k_k0[0], 0, Boundary_k_k0[0], Boundary_k_k0[1]};	//Number of gamma from center
 
 	long double F;			//Sum of ordinates*weights
 	long double Answer = 0;	//Results to be returned
@@ -453,18 +449,19 @@ long double k0_Int(long double Par[], int Temp, long double k, long double theta
 
 	long double zero[12];	//Real part of poles, up to 2 come from potential and up to 2 come from single quark spectrum
 	long double gamma[12];	//Imaginary part of poles
+	long double Caution = (Par[4]-pow(2.*Par[2],2)>0)?(Par[3]*cos(theta)/2.*sqrt((Par[4]-pow(2.*Par[2],2))/(Par[4]-pow(Par[3]*sin(theta),2)))):((Energy(0, Par[3]/2., k, theta)-Energy(0, Par[3]/2., -k, theta))/2.);	//Double on-shell in the k0 direction
 	int Poles = 0;		//Number of poles with real parts between 0 and E
 	int i, j, l;		//Counting varibles
 	int Intervals;		//Number of intervals required by poles and discontinuities
 
 	Characterize_k0_Int(Par, Temp, k, theta, zero, gamma, Poles);	//Get the poles that I have to be concerned about
-	long double Stops[Poles*17+6];					//Intervals that are required by integrating near poles
+	long double Stops[Poles*5+6];					//Intervals that are required by integrating near poles
 
 	l = 0;
 	for(i = 0; i < Poles; i++)
 	{
 		if(!isnan(gamma[i]) && !isnan(zero[i]))	//Prevents bad poles from getting in (It would be better to find the source of bad poles and eliminate it)
-			for(j = 0; j < 17; j++)
+			for(j = 0; j < 5; j++)
 			{
 				Stops[l] = zero[i]+gamma[i]*Range[j];	//Extra subintervals required by poles
 				l++;
@@ -537,36 +534,15 @@ long double k0_Int(long double Par[], int Temp, long double k, long double theta
 
 		F = 0;	//Zero out F for next sub-interval
 
-#if ORDER == 37
-		for(l = 0; l < 9; l++) //Count through points away from center
-#elif ORDER == 97
-		for(l = 0; l < 24; l++)
-#endif
+		for(j = 0; j < 2; j++)
 		{
-			x1 = (b+a-Disp[l]*(b-a))/2.;
-			x2 = (b+a+Disp[l]*(b-a))/2.;
+			x1 = (b+a-Disp9[j]*(b-a))/2.;
+			x2 = (b+a+Disp9[j]*(b-a))/2.;
 
-			F += Imk0_Integrand(Par,x1,k,theta,Temp)*w[l+1];
-			F += Imk0_Integrand(Par,x2,k,theta,Temp)*w[l+1];
+			F += Imk0_Integrand(Par,x1,k,theta,Temp)*w[j+1];
+			F += Imk0_Integrand(Par,x2,k,theta,Temp)*w[j+1];
 		}
 		F += Imk0_Integrand(Par,(a+b)/2.,k,theta,Temp)*w[0];
-
-		/*if(a >= Max && Temp != 0)
-		{
-#if ORDER == 37
-			for(l = 0; l < 9; l++) //Count through points away from center
-#elif ORDER == 97
-			for(l = 0; l < 24; l++)
-#endif
-			{
-				x1 = -(b+a-Disp[l]*(b-a))/2.;
-				x2 = -(b+a+Disp[l]*(b-a))/2.;
-
-				F += Imk0_Integrand(Par,x1,k,theta,Temp)*w[l+1];
-				F += Imk0_Integrand(Par,x2,k,theta,Temp)*w[l+1];
-			}
-			F += Imk0_Integrand(Par,-(a+b)/2.,k,theta,Temp)*w[0];
-		}*/
 
 		Partial = F*(b-a)/2.;
 		Answer += Partial;		//Add the subinterval to the total
