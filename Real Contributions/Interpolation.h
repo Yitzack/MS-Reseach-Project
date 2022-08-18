3363c6ae9e45e200aa4be5b4875f78375a854c3b
Yitzack <isarver@physics.tamu.edu>
2022-08-18 15:04:12 -0500

    Update to Interpolation.h
    Changes out function pointers for a numerical go this way to a function wrapper. Compiles under all versions of C++.

--- Real Contributions/Interpolation.h
+++ Real Contributions/Interpolation.h
@@ -1,6 +1,3 @@
-#include<array>
-#include<functional>
-
 template <class T>
 class Interpolation
 {
@@ -16,6 +13,7 @@ class Interpolation
 		long double Basis2(long double);
 		long double Basis3(long double);
 		long double Basisn(long double);
+		long double Basis_Wrapper(long double, int);
 };
 
 
@@ -43,106 +41,104 @@ Interpolation<T>::Interpolation(T** Control, int xSize, int ySize)	//I really wa
 template <class T>
 T Interpolation<T>::operator()(long double x, long double y)
 {
-	//long double (Interpolation<T>::*Basisi[4])(long double) = {&Interpolation<T>::Basisn, &Interpolation<T>::Basisn, &Interpolation<T>::Basisn, &Interpolation<T>::Basisn}; //Basis Functions in the i direction
-	//long double (Interpolation<T>::*Basisj[4])(long double) = {&Interpolation<T>::Basisn, &Interpolation<T>::Basisn, &Interpolation<T>::Basisn, &Interpolation<T>::Basisn}; //Basis Functions in the i direction
-	std::array Basisi{std::mem_fn(&Interpolation<T>::Basisn),std::mem_fn(&Interpolation<T>::Basisn),std::mem_fn(&Interpolation<T>::Basisn),std::mem_fn(&Interpolation<T>::Basisn)};
-	std::array Basisj{std::mem_fn(&Interpolation<T>::Basisn),std::mem_fn(&Interpolation<T>::Basisn),std::mem_fn(&Interpolation<T>::Basisn),std::mem_fn(&Interpolation<T>::Basisn)};
+	int Basisi[4] = {4,4,4,4}; //Basis Functions in the i direction
+	int Basisj[4] = {4,4,4,4}; //Basis Functions in the i direction
 	long double zx[4][4], zy[4][4];					 //z from the x direction and y direction
 	int offset_i = -1, offset_j = -1;					 //Index offsets in calling up control points, normally -1, but can be 0 or -2 on the ends
 	T Answer = T(0);
 
 	if(x < 2)	//Reassign the function pointers for the x/i direction
 	{
-		Basisi[0] = std::mem_fn(&Interpolation<T>::Basis0);
-		Basisi[1] = std::mem_fn(&Interpolation<T>::Basis1);
-		Basisi[2] = std::mem_fn(&Interpolation<T>::Basis2);
-		Basisi[3] = std::mem_fn(&Interpolation<T>::Basis3);
+		Basisi[0] = 0;
+		Basisi[1] = 1;
+		Basisi[2] = 2;
+		Basisi[3] = 3;
 		if(x < 1)
 			offset_i = 0;
 	}
 	else if(x < 3)
 	{
-		Basisi[0] = std::mem_fn(&Interpolation<T>::Basis1);
-		Basisi[1] = std::mem_fn(&Interpolation<T>::Basis2);
-		Basisi[2] = std::mem_fn(&Interpolation<T>::Basis3);
+		Basisi[0] = 1;
+		Basisi[1] = 2;
+		Basisi[2] = 3;
 	}
 	else if(x < 4)
 	{
-		Basisi[0] = std::mem_fn(&Interpolation<T>::Basis2);
-		Basisi[1] = std::mem_fn(&Interpolation<T>::Basis3);
+		Basisi[0] = 2;
+		Basisi[1] = 3;
 	}
 	else if(x < 5)
 	{
-		Basisi[0] = std::mem_fn(&Interpolation<T>::Basis3);
+		Basisi[0] = 3;
 	}
 	else if(xRange-2 <= x)
 	{
-		Basisi[0] = std::mem_fn(&Interpolation<T>::Basis3);
-		Basisi[1] = std::mem_fn(&Interpolation<T>::Basis2);
-		Basisi[2] = std::mem_fn(&Interpolation<T>::Basis1);
-		Basisi[3] = std::mem_fn(&Interpolation<T>::Basis0);
+		Basisi[0] = 3;
+		Basisi[1] = 2;
+		Basisi[2] = 1;
+		Basisi[3] = 0;
 	}
 	else if(xRange-3 <= x)
 	{
-		Basisi[1] = std::mem_fn(&Interpolation<T>::Basis3);
-		Basisi[2] = std::mem_fn(&Interpolation<T>::Basis2);
-		Basisi[3] = std::mem_fn(&Interpolation<T>::Basis1);
+		Basisi[1] = 3;
+		Basisi[2] = 2;
+		Basisi[3] = 1;
 	}
 	else if(xRange-4 <= x)
 	{
-		Basisi[2] = std::mem_fn(&Interpolation<T>::Basis3);
-		Basisi[3] = std::mem_fn(&Interpolation<T>::Basis2);
+		Basisi[2] = 3;
+		Basisi[3] = 2;
 	}
 	else if(xRange-5 <= x)
 	{
-		Basisi[3] = std::mem_fn(&Interpolation<T>::Basis3);
+		Basisi[3] = 3;
 	}
 
 	if(y < 2)	//Reassign the function pointers for the x/i direction
 	{
-		Basisj[0] = std::mem_fn(&Interpolation<T>::Basis0);
-		Basisj[1] = std::mem_fn(&Interpolation<T>::Basis1);
-		Basisj[2] = std::mem_fn(&Interpolation<T>::Basis2);
-		Basisj[3] = std::mem_fn(&Interpolation<T>::Basis3);
+		Basisj[0] = 0;
+		Basisj[1] = 1;
+		Basisj[2] = 2;
+		Basisj[3] = 3;
 		if(y < 1)
 			offset_j = 0;
 	}
 	else if(y < 3)
 	{
-		Basisj[0] = std::mem_fn(&Interpolation<T>::Basis1);
-		Basisj[1] = std::mem_fn(&Interpolation<T>::Basis2);
-		Basisj[2] = std::mem_fn(&Interpolation<T>::Basis3);
+		Basisj[0] = 1;
+		Basisj[1] = 2;
+		Basisj[2] = 3;
 	}
 	else if(y < 4)
 	{
-		Basisj[0] = std::mem_fn(&Interpolation<T>::Basis2);
-		Basisj[1] = std::mem_fn(&Interpolation<T>::Basis3);
+		Basisj[0] = 2;
+		Basisj[1] = 3;
 	}
 	else if(y < 5)
 	{
-		Basisj[0] = std::mem_fn(&Interpolation<T>::Basis3);
+		Basisj[0] = 3;
 	}
 	else if(yRange-2 <= y)
 	{
-		Basisj[0] = std::mem_fn(&Interpolation<T>::Basis3);
-		Basisj[1] = std::mem_fn(&Interpolation<T>::Basis2);
-		Basisj[2] = std::mem_fn(&Interpolation<T>::Basis1);
-		Basisj[3] = std::mem_fn(&Interpolation<T>::Basis0);
+		Basisj[0] = 3;
+		Basisj[1] = 2;
+		Basisj[2] = 1;
+		Basisj[3] = 0;
 	}
 	else if(yRange-3 <= y)
 	{
-		Basisj[1] = std::mem_fn(&Interpolation<T>::Basis3);
-		Basisj[2] = std::mem_fn(&Interpolation<T>::Basis2);
-		Basisj[3] = std::mem_fn(&Interpolation<T>::Basis1);
+		Basisj[1] = 3;
+		Basisj[2] = 2;
+		Basisj[3] = 1;
 	}
 	else if(yRange-4 <= y)
 	{
-		Basisj[2] = std::mem_fn(&Interpolation<T>::Basis3);
-		Basisj[3] = std::mem_fn(&Interpolation<T>::Basis2);
+		Basisj[2] = 3;
+		Basisj[3] = 2;
 	}
 	else if(yRange-5 <= y)
 	{
-		Basisj[3] = std::mem_fn(&Interpolation<T>::Basis3);
+		Basisj[3] = 3;
 	}
 
 	if(int(x)+3+offset_i >= xRange+1)
@@ -153,24 +149,48 @@ T Interpolation<T>::operator()(long double x, long double y)
 	for(int i_count = 3; i_count >= 0; i_count--)	//Evaluate the Basis Functions
 		for(int j_count = 3; j_count >= 0; j_count--)
 		{
-			if(Basisj[j_count] != std::mem_fn(&Interpolation<T>::Basisn) && y < 5)
-				zy[i_count][j_count] = Basisj[j_count](*this, y);
-			else if(Basisj[j_count] != std::mem_fn(&Interpolation<T>::Basisn) && yRange-5 <= y)
-				zy[i_count][j_count] = Basisj[j_count](*this, yRange-y);
+			if(Basisj[j_count] != 4 && y < 5)
+				zy[i_count][j_count] = Basis_Wrapper(y, Basisj[j_count]);
+			else if(Basisj[j_count] != 4 && yRange-5 <= y)
+				zy[i_count][j_count] = Basis_Wrapper(yRange-y, Basisj[j_count]);
 			else
-				zy[i_count][j_count] = Basisj[j_count](*this, y-offset[int(x)][int(y)][1]-j_count);	//Some how the fractional part of the arguments were trading 
+				zy[i_count][j_count] = Basis_Wrapper(y-offset[int(x)][int(y)][1]-j_count, Basisj[j_count]);	//Some how the fractional part of the arguments were trading 
 
-			if(Basisi[i_count] != std::mem_fn(&Interpolation<T>::Basisn) && x < 5)
-				zx[i_count][j_count] = Basisi[i_count](*this, x);
-			else if(Basisi[i_count] != std::mem_fn(&Interpolation<T>::Basisn) && xRange-5 <= x)
-				zx[i_count][j_count] = Basisi[i_count](*this, xRange-x);
+			if(Basisi[i_count] != 4 && x < 5)
+				zx[i_count][j_count] = Basis_Wrapper(x, Basisi[i_count]);
+			else if(Basisi[i_count] != 4 && xRange-5 <= x)
+				zx[i_count][j_count] = Basis_Wrapper(xRange-x, Basisi[i_count]);
 			else
-				zx[i_count][j_count] = Basisi[i_count](*this, x-offset[int(x)][int(y)][0]-i_count);
+				zx[i_count][j_count] = Basis_Wrapper(x-offset[int(x)][int(y)][0]-i_count, Basisi[i_count]);
 
 			Answer += zx[i_count][j_count]*zy[i_count][j_count]*control_points[int(x)+i_count+offset_i][int(y)+j_count+offset_j];
 		}
 
 	return(Answer);
+}
+
+template <class T>
+long double Interpolation<T>::Basis_Wrapper(long double x, int Basis)
+{
+	switch(Basis)
+	{
+	case 0:
+		return(Basis0(x));
+		break;
+	case 1:
+		return(Basis1(x));
+		break;
+	case 2:
+		return(Basis2(x));
+		break;
+	case 3:
+		return(Basis3(x));
+		break;
+	case 4:
+	default:
+		return(Basisn(x));
+		break;
+	}
 }
 
 template <class T>
